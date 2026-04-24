96e2a7909ffc60eda4dd432259529992691166e1
Daemo <craft.rom1@gmail.com>
2026-04-24 21:16:19 +0200

    Updated all 42 Headers, replaced settings refresh every frame with a refresh when run is called

--- src/app/loop/in_out.c
+++ src/app/loop/in_out.c
@@ -6,17 +6,18 @@
 /*   By: rprieur <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 12:55:33 by rprieur           #+#    #+#             */
-/*   Updated: 2026/03/19 12:55:33 by rprieur          ###   ########.fr       */
+/*   Updated: 2026/04/24 16:11:15 by rprieur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "veclc.h"
 #include "modules/mbx_inputs.h"
+#include "modules/mbx_handlers.h"
 #include "../../_private/mbx_internal.h"
 
 void	enter_loop(t_mbx *mbx)
 {
 	update_time_values(mbx);
+	mbx_refresh_settings(mbx);
 	if (mbx->settings.lock_cursor)
 		mbx_warp_cursor(mbx, vec2i_div_d(mbx->viewport->size, 2));
 }
--- src/app/loop/start_end.c
+++ src/app/loop/start_end.c
@@ -6,13 +6,12 @@
 /*   By: daemo <daemo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/27 20:52:30 by daemo             #+#    #+#             */
-/*   Updated: 2026/01/24 00:09:16 by rprieur          ###   ########.fr       */
+/*   Updated: 2026/04/24 16:11:15 by rprieur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "modules/mbx_drawing.h"
 #include "modules/mbx_inputs.h"
-#include "modules/mbx_handlers.h"
 #include "../../_private/mbx_internal.h"
 
 void	mbx_start_frame(t_mbx *mbx)
@@ -27,6 +26,5 @@ void	mbx_end_frame(t_mbx *mbx)
 	mbx_render_region_as_viewport(mbx,
 		mbx->viewport, mbx->settings.viewport_render);
 	mbx_flush_inputs(mbx);
-	mbx_refresh_settings(mbx);
 	update_time_values(mbx);
 }
